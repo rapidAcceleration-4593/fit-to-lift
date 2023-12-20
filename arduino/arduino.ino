0b8ccb2d4d783d272c0cc3d88620f2bf4619ce98
mathias314 <mathewclutter@gmail.com>
2023-12-20 15:13:28 -0700

    motor up and down wow

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,40 +1,68 @@
 // pin connections
 int loadCellAnalog = A1;
-int motorControllerRightEnable = 7;
-int motorControllerLeftEnable = 8;
-int motorRPWM = 5;
-int motorLPWM = 6; 
+int motorRightEnable = 7;
+int motorLeftEnable = 8;
+int motorRightPWM = 6;
+int motorLeftPWM = 5; 
 int magnetA = 12;
 int magnetB = 13;
 int encoderA = 3;
 int encoderB = 4;
 
 void setup() {
-  pinMode(motorControllerRightEnable, OUTPUT);
-  pinMode(motorControllerLeftEnable, OUTPUT);
-  pinMode(motorRPWM, OUTPUT);
-  pinMode(motorLPWM, OUTPUT);
+  pinMode(motorRightEnable, OUTPUT);
+  pinMode(motorLeftEnable, OUTPUT);
+  pinMode(motorRightPWM, OUTPUT);
+  pinMode(motorLeftPWM, OUTPUT);
   pinMode(magnetA, OUTPUT);
   pinMode(magnetB, OUTPUT);
   
   // encoder pin modes, in/out?
 
-  digitalWrite(motorControllerRightEnable, LOW);
-  digitalWrite(motorControllerLeftEnable, LOW);
+  digitalWrite(motorRightEnable, HIGH);
+  digitalWrite(motorLeftEnable, HIGH);
+
+  analogWrite(motorLeftPWM, 0);
+  analogWrite(motorRightPWM, 0);
 
   digitalWrite(magnetA, LOW);
   digitalWrite(magnetB, LOW);
 }
 
 void loop() {
-  digitalWrite(magnetA, HIGH);
-  digitalWrite(magnetB, HIGH);
+  //digitalWrite(magnetA, HIGH);
+  //digitalWrite(magnetB, HIGH);
+
+  motorUp();
 
   delay(1000);
 
   digitalWrite(magnetA, LOW);
   digitalWrite(magnetB, LOW);
 
+  motorStop();
+
+  motorDown();
+
   delay(1000);
 
+}
+
+void motorDown() {
+  digitalWrite(motorRightEnable, HIGH);
+  digitalWrite(motorLeftEnable, HIGH);
+  analogWrite(motorRightPWM, 100);
+  analogWrite(motorLeftPWM, 0);
+}
+
+void motorUp() {
+  digitalWrite(motorRightEnable, HIGH);
+  digitalWrite(motorLeftEnable, HIGH);
+  analogWrite(motorRightPWM, 0);
+  analogWrite(motorLeftPWM, 100);
+}
+
+void motorStop() {
+  digitalWrite(motorRightEnable, LOW);
+  digitalWrite(motorLeftEnable, LOW);
 }
